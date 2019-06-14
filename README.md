7379563f8ed411d84f055e703dd154d86b842638
msphar <michael.sphar@braintreepayments.com>
2019-06-14 16:03:50 +0000

    Don't look at it, just...seriously don't look at it.
    Yes, I don't know what I'm doing and this code probably looks awful.
    
    But it does happily generate way too many log messages.

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # logstorm
-A very bad go program for generating way too many log messages
+A very bad go program for generating way too many log messages and blasts them at the local host syslog port (UDP 514)
+
+Usage:
+  -rate int
+        Number of messages each worker will generate every second (default 1)
+  -workers int
+        Number of workers that will simultaneously generate log messages (default 1)
+
+Will generate (potentially very large amounts of) messages that look like this:
+
+```
+test log message from ./generate_logs: worker 2 at 2019-06-14 15:56:31.502922819 +0000 UTC m=+3.001445236
+```
---
+++ generate_logs.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net"
+	"os"
+	"sync"
+	"time"
+)
+
+func udp_send_measure(conn net.Conn, worker int) time.Duration {
+	start := time.Now()
+	fmt.Fprintf(conn, "test log message from %s: worker %d at %s", os.Args[0], worker, start)
+	elapsed := time.Since(start)
+	return elapsed
+}
+
+func udp_loop(target string, worker int, msg_rate int, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	conn, err := net.Dial("udp", target)
+	defer conn.Close()
+	if err != nil {
+		fmt.Println("Error opening connecton:  %v", err)
+		return
+	}
+
+	for {
+		for i := 0; i < msg_rate; i++ {
+			_ = udp_send_measure(conn, worker)
+		}
+		time.Sleep(time.Second)
+	}
+}
+
+func main() {
+
+	port := "514"
+	target := "127.0.0.1"
+
+	var msg_rate int
+	var workers int
+	flag.IntVar(&msg_rate, "rate", 1, "Number of messages each worker will generate every second")
+	flag.IntVar(&workers, "workers", 1, "Number of workers that will simultaneously generate log messages")
+
+	flag.Parse()
+
+	fmt.Printf("Spawning %d workers to each generate %d log messages every second\n", workers, msg_rate)
+
+	var wg sync.WaitGroup
+	wg.Add(workers)
+
+	for i := 0; i < workers; i++ {
+		go udp_loop(target+":"+port, i, msg_rate, &wg)
+	}
+
+	wg.Wait()
+}
