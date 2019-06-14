a6b4a3c925b018bc04bf9d1777b8982e51ec8293
msphar <michael.sphar@braintreepayments.com>
2019-06-14 18:58:54 +0000

    Add flags for source and test message
    plus other various alleged improvements

--- logstorm.go
+++ logstorm.go
@@ -1,22 +1,15 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
 	"net"
-	"os"
 	"sync"
 	"time"
 )
 
-func udp_send_measure(conn net.Conn, worker int) time.Duration {
-	start := time.Now()
-	fmt.Fprintf(conn, "test log message from %s: worker %d at %s", os.Args[0], worker, start)
-	elapsed := time.Since(start)
-	return elapsed
-}
-
-func udp_loop(target string, worker int, msg_rate int, wg *sync.WaitGroup) {
+func send_logs(target string, worker int, msg_rate int, source string, message string, wg *sync.WaitGroup) {
 	defer wg.Done()
 
 	conn, err := net.Dial("udp", target)
@@ -26,8 +19,22 @@ func udp_loop(target string, worker int, msg_rate int, wg *sync.WaitGroup) {
 		return
 	}
 
+	type LogMessage struct {
+		Source    string
+		Worker    int
+		Message   string
+		Timestamp time.Time
+	}
+
 	for {
-		_ = udp_send_measure(conn, worker)
+		raw_message := LogMessage{
+			Source:    source,
+			Worker:    worker,
+			Message:   message,
+			Timestamp: time.Now(),
+		}
+		json_message, _ := json.Marshal(raw_message)
+		fmt.Fprintf(conn, string(json_message))
 		time.Sleep(time.Duration(1000000000 / msg_rate))
 	}
 }
@@ -37,20 +44,25 @@ func main() {
 	port := "514"
 	target := "127.0.0.1"
 
-	var msg_rate int
-	var workers int
+	var msg_rate, workers int
+	var source, message string
+
 	flag.IntVar(&msg_rate, "rate", 1, "Number of messages each worker will generate every second")
 	flag.IntVar(&workers, "workers", 1, "Number of workers that will simultaneously generate log messages")
+	flag.StringVar(&source, "source", "logstorm", "String identifying the source of the log messages")
+	flag.StringVar(&message, "message", "Test Message", "Message payload for every log message")
 
 	flag.Parse()
 
 	fmt.Printf("Spawning %d workers to each generate %d log messages every second\n", workers, msg_rate)
+	fmt.Printf("Message source: %s\n", source)
+	fmt.Printf("Message text: %s\n", message)
 
 	var wg sync.WaitGroup
 	wg.Add(workers)
 
 	for i := 0; i < workers; i++ {
-		go udp_loop(target+":"+port, i, msg_rate, &wg)
+		go send_logs(target+":"+port, i, msg_rate, source, message, &wg)
 	}
 
 	wg.Wait()
