a6e9416f1bd58fdc10ac47bf7d793316075e2a0d
Michael Sphar <michael.sphar@braintreepayments.com>
2019-07-19 21:15:30 +0000

    Add -count option to limit the number of messages
    Add a -count parameter to optionally limit the output to a specific
    number of messages per worker.
    
    This could be handy for testing when one wants to confirm that all
    messages are being delivered. Can send a specific large number of
    messages and count how many arrive at the endpoint.

--- logstorm.go
+++ logstorm.go
@@ -10,7 +10,7 @@ import (
 	"time"
 )
 
-func send_logs(target string, worker int, msg_rate int, source string, message string, json_out bool, wg *sync.WaitGroup) {
+func send_logs(target string, worker int, msg_rate int, msg_count int, source string, message string, json_out bool, wg *sync.WaitGroup) {
 	defer wg.Done()
 
 	sysLog, err := syslog.Dial("udp", target,
@@ -26,7 +26,13 @@ func send_logs(target string, worker int, msg_rate int, source string, message s
 		Timestamp time.Time
 	}
 
-	for {
+	var infinite bool = false
+
+	if msg_count < 0 {
+		infinite = true
+	}
+
+	for i := 0; i < msg_count || infinite; i++ {
 		raw_message := LogMessage{
 			Source:    source,
 			Worker:    worker,
@@ -43,6 +49,7 @@ func send_logs(target string, worker int, msg_rate int, source string, message s
 
 		time.Sleep(time.Duration(1000000000 / msg_rate))
 	}
+	return
 }
 
 func main() {
@@ -50,7 +57,7 @@ func main() {
 	port := "514"
 	target := "127.0.0.1"
 
-	var msg_rate, workers int
+	var msg_rate, workers, msg_count int
 	var source, message string
 	var json_out bool
 
@@ -58,6 +65,7 @@ func main() {
 	flag.IntVar(&workers, "workers", 1, "Number of workers that will simultaneously generate log messages")
 	flag.StringVar(&source, "source", "logstorm", "String identifying the source of the log messages")
 	flag.StringVar(&message, "message", "Test Message", "Message payload for every log message")
+	flag.IntVar(&msg_count, "count", -1, "Number of messages to generate per worker (-1 for unlimited)")
 	flag.BoolVar(&json_out, "json", false, "Format message as json")
 
 	flag.Parse()
@@ -65,6 +73,13 @@ func main() {
 	fmt.Printf("Spawning %d workers to each generate %d log messages every second\n", workers, msg_rate)
 	fmt.Printf("Message source: %s\n", source)
 	fmt.Printf("Message text: %s\n", message)
+	fmt.Printf("Total per worker: ")
+
+	if msg_count == 0 {
+	  fmt.Printf("Unlimited\n")
+	} else {
+	  fmt.Printf("%d\n", msg_count)
+	}
 
 	if json_out {
 		fmt.Printf("Format: json \n")
@@ -75,7 +90,7 @@ func main() {
 	wg.Add(workers)
 
 	for i := 0; i < workers; i++ {
-		go send_logs(target+":"+port, i, msg_rate, source, message, json_out, &wg)
+		go send_logs(target+":"+port, i, msg_rate, msg_count, source, message, json_out, &wg)
 	}
 
 	wg.Wait()
